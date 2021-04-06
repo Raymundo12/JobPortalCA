fabfa70f6827fef24325856667fa31ebb77a9e7a
StephaTech <stephatech123@gmail.com>
2021-04-06 03:08:18 +0000

    week 3(api)

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const http = require('http');
-const axios = require('axios');
+const axios = require('axios'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser');
 
-http.createServer((req, res)=>{
-//res.write("Hello world, Helo Steph you got it right this time!!!uhuuUu \n"); // write a response
-res.write(users.join("\n"));//displays the list of users on the page/SEPARATOR
-res.write(emails.join("\n\n"));
-res.end(); //end the response
-}).listen(8000); // listen for requests on port 8000
+var app = express();
+var port = 8000;
 
+app.use(bodyParser.json())//JSON
+
+// //option one setting routes
+// app.get('/hello', (req, res) => {//type  on the browse exactly what you wrote here
+//     res.json({message: "Hello yahh" });//name=message value=Hello yahh
+//     //res.send("Hello yahh");//changed it to SEND
+//     //res.send(users.join('\n'));
+// });
+//**********************************************************************************
+//option 2 - setting routes and passing parameters
+app.get('/hello/:foo/:bar', (req, res) => {//type  on the browse exactly what you wrote here
+    res.json({message: "Hello yahh", data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+});
+
+// http.createServer((req, res)=>{
+// //res.write("Hello world, Helo Steph you got it right this time!!!uhuuUu \n"); // write a response
+// res.write(users.join("\n"));//displays the list of users on the page/SEPARATOR
+// //res.write(emails.join("\n\n"));
+// res.end(); //end the response
+// }).listen(8000); // listen for requests on port 8000
+//**********************************************************
 // let users = [];//names of users will be stored here
 // axios.get("https://jsonplaceholder.typicode.com/users")//sending get request
 // .then(({ data }) => {
@@ -18,14 +42,21 @@ res.end(); //end the response
 //     console.log(error);
 // });
 let users = [];//names of users will be stored here
-let emails = [];
+//let emails = [];
 (async function getNames(){//syncronized minimize the errors and delays
     try{
-        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
-        users = data.map(user=>user.name);//map=populate to the users
-        emails = data.map(email=>email.email);//map=populate to the users
+        //const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");//external server
+        const {data} = await axios.get("https://swapi.dev/api/people/");//star wars //external server
+
+
+        users = data.results.map(user=>user.name);//map=populate to the users
+        //emails = data.map(email=>email.email);//map=populate to the users
        console.log(users)//print to the browser
     }catch (error) {
         console.log(error);
     }
 }) ();
+
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
