eb7b72fbefaf84be9d83cc173758dfc61ff98dea
StephaTech <stephatech123@gmail.com>
2021-04-08 15:07:25 +0000

    week6 setting mongo

--- routes.js
+++ routes.js
@@ -1,18 +1,27 @@
 const express = require('express'),
 router = express.Router();
 
+//reference into routes from the request logic intem-controller
+var itemCtrl = require('./item-controller');
+
+router.get('/hello', itemCtrl.getWorld);
+
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
+
+router.post('/hello', itemCtrl.postWorld);
 
 //option 2 - setting routes and passing parameters//for put at the front end
-router.get('/hello/:foo/:bar', (req, res) => {//type  on the browse exactly what you wrote here
-    res.json({message: "Hello yahh", data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-});
+// router.get('/hello/:foo/:bar', (req, res) => {//type  on the browse exactly what you wrote here
+//     res.json({message: "Hello yahh", data: [
+//         req.params.foo,
+//         req.params.bar
+//     ]});
+// });
 
-//post using postman//sending some data
-router.post('/hello',(req,res) => {
-    res.json({result: 'Post was sent',data: req.body});
-});
+// //post using postman//sending some data
+// router.post('/hello',(req,res) => {
+//     res.json({result: 'Post was sent',data: req.body});
+// });
 
 module.exports = router;//exports
+
