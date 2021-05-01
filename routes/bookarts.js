356fc17460ca4ca165871249a7c89ced9fd209c5
StephaTech <stephatech123@gmail.com>
2021-05-01 23:21:09 +0000

    CRUD-router/controlling setted and working

--- routes/bookarts.js
+++ routes/bookarts.js
@@ -1,54 +1,54 @@
+//dependencies modules
 const express = require('express')
 const Book = require('../models/books')//books is where my schema is
 const router = express.Router()
-
-//CRUD
+//This are APIs that node.js express server exports, learned in class 
+//Routes and Controller for handling all CRUD = Create, Read, Update, Delete/POST/GET/PUT/DELETE
 //this call view/bookart/new 
 router.get('/new', (req, res) => {
-    res.render('bookart/new', {part: new Book() })//book input is passed to the database collection model.
+    res.render('bookart/new', {part: new Book() })
 })
 
-router.get('/edit/:id', async (req, res) => {// defining new route to edit 
-  const part = await Book.findById(req.params.id) // passing the user selected id to the database collection and the document is found using 
-  // id and render to the edit page all the user selected id's data.
+router.get('/edit/:id', async (req, res) => { 
+  const part = await Book.findById(req.params.id) 
+  
   res.render('bookart/edit', { part: part })
 })
 
-
-router.get('/:id', async (req, res) => {// getting a car part details from database collection using id
+router.get('/:id', async (req, res) => {
   const part = await Book.findById()
   if (part == null) { // checking if part is null. if true sending a status code 404 and a error message.
- return res.status(404).json({message: 'Cannot find Book by id'})
+ return res.status(404).json({message: 'Ops! cant find this book by id'})
   } 
-  res.redirect('/')// redirect user to the homepage after the id is found
+  res.redirect('/')// if succesful, redirects user to the bookpage 
    
 })
 
-// CREATE BOOK.
+// CREATE AND SAVE BOOK.
 
 router.post('/', async (req, res, next) => {
-  req.part = new Book() // this is passed on to the database schema and new record is created.
+  req.part = new Book() 
   next()
   }, saveBooktAndRedirect('new')) // calling the function to create new record
 
 
-// put is used to update a record in the collection
-// a put request is sent using the document id to the database and the document 
+// request UPDATE/ PUT  by id
 router.put('/:id', async (req, res, next) => { 
-  req.part = await Book.findById(req.params.id) // finding the document with the help of id request by user.
+  req.part = await Book.findById(req.params.id) 
   next()
 
 }, saveBooktAndRedirect('edit')) // calling the function to edit the document
 
 
-//delete request is used to deleted a document
-router.delete('/:id', async (req, res)=> {  // getting the document id from the user
-    await Book.findByIdAndDelete(req.params.id)// finding the document and deleting the document from the collection
-    res.redirect('/#Audiobook')// rediricting the user to products page.
+//request DELETE by id
+router.delete('/:id', async (req, res)=> { 
+    await Book.findByIdAndDelete(req.params.id)
+    res.redirect('/#Audiobook')
 
 })
 
-function saveBooktAndRedirect(path) { // this function is used to pass all the user input to schema to create/ update
+//function UPDATE/ PUT/ CREATE BOOK on my web book page and my mongo db atlas
+function saveBooktAndRedirect(path) { 
   return async (req, res) => {
     let part = req.part
     part.item = req.body.item
@@ -58,10 +58,10 @@ function saveBooktAndRedirect(path) { // this function is used to pass all the u
     try {
       part = await part.save() // everything is saved to the Rest database collection
 
-     res.redirect(`/#Audiobook`)// redirect user to the product page .
+     res.redirect(`/#Audiobook`)
     } catch (e) {
  
- return res.status(400).json({message: 'Ops! Validation fail for the request'}) // sending a status code 400 if we have a bad request.
+ return res.status(400).json({message: 'Ops! Validation fail for the request'}) // in case runs in a error.
     }
   }
 }
