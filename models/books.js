dfa07db4e5f1cdda6f4e09bfe7c83007577a93b2
StephaTech <stephatech123@gmail.com>
2021-05-01 23:45:03 +0000

    installed heroku dependencie

--- models/books.js
+++ models/books.js
@@ -1,5 +1,5 @@
 //his is my model data representing my collection in mongo atlas called books..and the fields below will be generated automatically
-var mongoose = require('mongoose');
+var mongoose = require('mongoose');//to interact with the MongoDBatlas database called books 
 
 var bookSchema = new mongoose.Schema({ 
     item: { type: String, unique: true},
