1a7c0adb3052dfbd0af8e3c4d8eab6deca05b3d3
StephaTech <stephatech123@gmail.com>
2021-05-01 23:22:45 +0000

    updated

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ Title: moodle live classes
 Author:Mikhael
  title: How To Build A Markdown Blog Using Node.js, Express, And MongoDB 
 Author: Web Dev Simplified Availability: https://www.youtube.com/watch?v=1NrHkjlWVhM*/
-//dependencies,references
+//dependencies,modules references
 const http = require('http');
 //const axios = require('axios');
 logger = require('morgan');
-cors = require('cors');
-bodyParser = require('body-parser');
+cors = require('cors');//"provides Express middleware to enable CORS with various options"
+bodyParser = require('body-parser');//"helps to parse the request and create the req.body object"
 mongoose = require('mongoose');
 multer = require('multer');
 const dotenv = require('dotenv');
-express = require('express');//const
+express = require('express');//"is for building the Rest apis"
 const Book = require('./models/books')//books is where my schema is
 
+//MIDDLEWARES
 var app = express();
 const bookartRouter = require('./routes/bookarts')//bookarts js class
 const methodOverride = require('method-override')// package used to update and delete a document via post request.
@@ -22,30 +23,28 @@ app.use(methodOverride('_omethod'))// using the method in the app.//edit class/ 
 
 //INSTANCES listen and push
 app.use(express.urlencoded({ extended: false}))//enable us to use URL-encoded data with the querystring library.
-var port = process.env.PORT || 8000;//port default
+var port = process.env.PORT || 8000;//set port, listen for incoming requests + for heroku deployment
 dotenv.config();
-//MIDDLEWARES
+
 app.set('view engine', 'ejs')
 app.use('/bookarts', bookartRouter)//bookarts js class import export
-
-
 
 app.use(express.static('views/bookart')) // express static makes my folder public to access my css and javascript for my html page.
 
 //making a get request html page
 app.get('/', async  (req,res)=> {
-   const partsbooks = await Book.find();  // find my data from my REST database collection with the model and assign it on to the const Book
+const partsbooks = await Book.find();  // find my data from my REST database collection with the model and assign it on to the const Book
 
     res.render('bookart/index' , {partsbooks: partsbooks})// This will render all the database collection data which was assigned on to the const Book
 })
 
-app.use(bodyParser.json())//JSON /first
+app.use(bodyParser.json())// parse requests of content-type - application/json
 app.use(logger('tiny'));//morgan//knows the n point of requests and responses between the user and controller: GET / 404 139 - 1.760 ms//POST /hello 200 126 - 1.586 ms
 //app.use(require('./routes'));//last
 
 
 app.listen(port, function(err){
-    console.log('Listening on port: ' + port);//my listening
+    console.log('Listening on port: ' + port);//set port, listen for requests
 });
 
 const dbURI = process.env.DB_URL;
